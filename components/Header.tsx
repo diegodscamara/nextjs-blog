ebc435b8b995053fbea303170ae06b14d9d88fbc
Diego Câmara <diegodscamara@gmail.com>
2024-04-14 17:48:00 -0300

    Fix import order and update image styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,12 +11,12 @@ const Header = () => {
     <header className="flex items-center justify-between py-10">
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between rounded-lg p-2 transition-all hover:bg-slate-100 dark:hover:bg-slate-900">
             <div className="mr-3">
               <Logo />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-auto text-2xl font-semibold sm:block">
+              <div className="hidden h-auto text-lg font-semibold sm:block">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -16,9 +16,8 @@ const discussUrl = (path) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(`${siteMetadata.siteUrl}/${path}`)}`
 
 const postDateTemplate: Intl.DateTimeFormatOptions = {
-  weekday: 'long',
   year: 'numeric',
-  month: 'long',
+  month: 'short',
   day: 'numeric',
 }
 
@@ -41,28 +40,15 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
             <div className="space-y-1 text-center">
-              <dl className="space-y-10">
-                <div>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
-                  </dd>
-                </div>
-              </dl>
               <div>
                 <PageTitle>{title}</PageTitle>
               </div>
-              <span className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                {readingTime.text}
-              </span>
             </div>
           </header>
           <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
             <dl className="pb-10 pt-6 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
               <dt className="sr-only">Authors</dt>
-              <dd>
+              <dd className="flex flex-row flex-wrap items-center justify-between gap-2">
                 <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
                   {authorDetails.map((author) => (
                     <li className="flex items-center space-x-2" key={author.name}>
@@ -78,19 +64,22 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                       <dl className="whitespace-nowrap text-sm font-medium leading-5">
                         <dt className="sr-only">Name</dt>
                         <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
-                        <dd>
-                          {author.twitter && (
-                            <Link
-                              href={author.twitter}
-                              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            >
-                              {author.twitter
-                                .replace('https://twitter.com/', '@')
-                                .replace('https://x.com/', '@')}
-                            </Link>
-                          )}
-                        </dd>
+                        <div className="flex items-center gap-1">
+                          <dt className="sr-only">Reading time</dt>
+                          <dd className="text-center text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                            {readingTime.text}
+                          </dd>
+                          {` • `}
+                          <dt className="sr-only">Published on</dt>
+                          <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                            <time dateTime={date}>
+                              {new Date(date).toLocaleDateString(
+                                siteMetadata.locale,
+                                postDateTemplate
+                              )}
+                            </time>
+                          </dd>
+                        </div>
                       </dl>
                     </li>
                   ))}
