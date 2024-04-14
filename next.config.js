18a152e93a550d915936496621f17d3dfde9f30e
Diego Câmara <diegodscamara@gmail.com>
2024-04-14 15:16:29 -0300

    Update Content Security Policy and headers configuration

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app us.umami.is 'https://www.google-analytics.com' 'https://analytics.umami.is' 'https://www.googletagmanager.com';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app us.umami.is https://www.google-analytics.com https://analytics.umami.is https://www.googletagmanager.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src 'self' giscus.app;
 `
 
 const securityHeaders = [
@@ -76,8 +76,19 @@ module.exports = () => {
     async headers() {
       return [
         {
-          source: '/(.*)',
-          headers: securityHeaders,
+          source: '/:path*',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+            },
+            { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+            { key: 'X-Frame-Options', value: 'DENY' },
+            { key: 'X-Content-Type-Options', value: 'nosniff' },
+            { key: 'X-DNS-Prefetch-Control', value: 'on' },
+            { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' },
+            { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
+          ],
         },
       ]
     },
