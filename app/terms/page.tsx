76650d78eb0130fab6885f4b667b46fe3debb1b6
Diego Câmara <diegodscamara@gmail.com>
2024-04-16 21:39:47 -0300

    Update tag data and remove blog post

---
+++ app/terms/page.tsx
@@ -0,0 +1,108 @@
+export default function TermsPage() {
+  return (
+    <div className="mx-auto max-w-4xl p-4">
+      <h1 className="my-6 text-center text-3xl font-bold">Terms and Conditions</h1>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">1. Introduction</h2>
+      <p className="mb-4 text-base">
+        Welcome to Diego Câmara's Blog. By accessing our website, you agree to be bound by these
+        terms and conditions. Please read them carefully. If you do not agree with these terms, you
+        should not use this website.
+      </p>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">2. Intellectual Property Rights</h2>
+      <p className="mb-4 text-base">
+        Unless otherwise stated, Diego Câmara's Blog and/or its licensors own the intellectual
+        property rights for all material on Diego Câmara's Blog. All intellectual property rights
+        are reserved. You may access this from Diego Câmara's Blog for your own personal use
+        subjected to restrictions set in these terms and conditions.
+      </p>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">3. Restrictions</h2>
+      <p className="mb-2 text-base">You are specifically restricted from the following:</p>
+      <ul className="mb-4 ml-8 list-disc">
+        <li>Publishing any website material in any media without prior written consent.</li>
+        <li>Selling, sublicensing, and/or otherwise commercializing any website material.</li>
+        <li>Publicly performing and/or showing any website material.</li>
+        <li>
+          Using this website in any way that is damaging, or that could be damaging to this website.
+        </li>
+        <li>Using this website in any way that impacts user access to this website.</li>
+        <li>
+          Using this website contrary to applicable laws and regulations, or in a way that causes,
+          or may cause, harm to the website, or to any person or business entity.
+        </li>
+      </ul>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">4. User Content</h2>
+      <p className="mb-4 text-base">
+        In these terms and conditions, "User Content" means any audio, video, text, images, or other
+        material you choose to display on this website. By displaying it, you grant Diego Câmara's
+        Blog a non-exclusive, worldwide, irrevocable license to use, reproduce, adapt, publish,
+        translate, and distribute it in any and all media.
+      </p>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">5. No warranties</h2>
+      <p className="mb-4 text-base">
+        This website is provided "as is," with all faults, and Diego Câmara's Blog makes no express
+        or implied representations or warranties, of any kind related to this website or the
+        materials contained on this website. Additionally, nothing contained on this website shall
+        be construed as providing consult or advice to you.
+      </p>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">6. Limitation of liability</h2>
+      <p className="mb-4 text-base">
+        In no event shall Diego Câmara's Blog, nor any of its officers, directors, and employees, be
+        held liable for anything arising out of or in any way connected with your use of this
+        website whether such liability is under contract. Diego Câmara's Blog, including its
+        officers, directors, and employees shall not be held liable for any indirect, consequential,
+        or special liability arising out of or in any way related to your use of this website.
+      </p>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">7. Indemnification</h2>
+      <p className="mb-4 text-base">
+        You hereby indemnify to the fullest extent Diego Câmara's Blog from and against any and/or
+        all liabilities, costs, demands, causes of action, damages, and expenses arising in any way
+        related to your breach of any of the provisions of these terms and conditions.
+      </p>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">8. Severability</h2>
+      <p className="mb-4 text-base">
+        If any provision of these terms and conditions is found to be invalid under any applicable
+        law, such provisions shall be deleted without affecting the remaining provisions herein.
+      </p>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">9. Variation of Terms</h2>
+      <p className="mb-4 text-base">
+        Diego Câmara's Blog is permitted to revise these terms and conditions at any time as it sees
+        fit, and by using this website you are expected to review such terms and conditions on a
+        regular basis to ensure you understand all terms and conditions governing use of this
+        website.
+      </p>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">10. Assignment</h2>
+      <p className="mb-4 text-base">
+        Diego Câmara's Blog is allowed to assign, transfer, and subcontract its rights and/or
+        obligations under these terms and conditions without any notification or consent required.
+        However, you shall not be permitted to assign, transfer, or subcontract any of your rights
+        and/or obligations under these terms and conditions.
+      </p>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">11. Entire Agreement</h2>
+      <p className="mb-4 text-base">
+        These terms and conditions, including any legal notices and disclaimers contained on this
+        website, constitute the entire agreement between Diego Câmara's Blog and you in relation to
+        your use of this website, and supersede all prior agreements and understandings with respect
+        to the same.
+      </p>
+
+      <h2 className="mb-2 mt-6 text-2xl font-semibold">Governing Law & Jurisdiction</h2>
+      <p className="mb-4 text-base">
+        These terms will be governed by and interpreted in accordance with the laws of the
+        jurisdiction in which Diego Câmara's Blog operates, and you submit to the non-exclusive
+        jurisdiction of the state and federal courts located in Canada for the resolution of any
+        disputes.
+      </p>
+    </div>
+  )
+}
